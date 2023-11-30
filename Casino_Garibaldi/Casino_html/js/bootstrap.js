3ac625351c9a16a973c7bdb6b43ce3974784f517
Andrés Lombana <felipelombana2@gmail.com>
2023-11-29 19:04:08 -0500

    Casino Garibaldi

--- Casino_Garibaldi/Casino_html/js/bootstrap.js
+++ Casino_Garibaldi/Casino_html/js/bootstrap.js
@@ -4,6 +4,10 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
 
+setTimeout(function () {
+  $('.loader_bg').fadeToggle();
+}, 2000);
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')) :
     typeof define === 'function' && define.amd ? define(['exports', 'jquery', 'popper.js'], factory) :
